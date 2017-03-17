befcad96846a6006694432396ba599db3d13a709
oleg <olegjs@users.noreply.github.com>
2017-03-17 05:02:43 -0700

    add closest as dependency

--- delegate.js
+++ delegate.js
@@ -1,4 +1,5 @@
 window.Rd = window.Rd || {}
+
 window.Rd.delegateEvent = function (delegateNode, eventType, selector, listener) {
   delegateNode.addEventListener(eventType, function (event) {
     var match = event.target.closest(selector)
